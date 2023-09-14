19d89368dda9ae2273916f9da0d035f010f79e3c
FIRST_NAME LAST_NAME <nitin0969.be21@chitkara.edu.in>
2023-09-14 09:25:03 +0530

    c++

---
+++ 135. Candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+            int n = ratings.size();
+            vector<int>left(n,1);
+            vector<int>right(n,1);
+            for(int i = 1;i<n;i++){
+                if(ratings[i]>ratings[i-1]){
+                    left[i] = left[i-1]+1;
+                }
+            }
+            for(int j = n-2 ; j>=0 ; --j){
+                if(ratings[j] > ratings[j+1]){
+                    right[j] = right[j+1]+1;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < n; ++i) {
+                sum += max(left[i], right[i]);
+            }       
+            return sum;
+    }
+};
